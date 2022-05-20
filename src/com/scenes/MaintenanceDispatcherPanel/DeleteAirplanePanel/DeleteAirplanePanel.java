1a2f433659f3ca92ba5943bd6c248710f7a0fbba
HachikoR <deniszhukov.hachiko@gmail.com>
2022-05-20 17:59:55 +0300

    fast save

--- src/com/scenes/MaintenanceDispatcherPanel/DeleteAirplanePanel/DeleteAirplanePanel.java
+++ src/com/scenes/MaintenanceDispatcherPanel/DeleteAirplanePanel/DeleteAirplanePanel.java
@@ -1,7 +1,6 @@
 package com.scenes.MaintenanceDispatcherPanel.DeleteAirplanePanel;
 
 import com.assets.services.InteractingWithWindow;
-import com.scenes.MaintenanceDispatcherPanel.AddAirplanePanel.AddAirplanePanel;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 
@@ -9,9 +8,12 @@ import javafx.stage.Stage;
 public class DeleteAirplanePanel {
     public static void showModal() {
         Stage stage = new Stage();
-        FXMLLoader loader = new FXMLLoader(AddAirplanePanel.class.getResource("AddAirplanePanel.fxml"));
+        FXMLLoader loader = new FXMLLoader(DeleteAirplanePanel.class.getResource("DeleteAirplanePanel.fxml"));
         InteractingWithWindow.showModal(stage, loader);
         stage.centerOnScreen();
+
+        //ЗАГРУЗКА НОМЕРОВ И ID
+        stage.getScene().lookup("#numberAirplainComboBox");
 
         stage.show();
     }
